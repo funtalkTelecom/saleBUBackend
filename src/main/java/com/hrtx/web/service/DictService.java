3350e7b157b7d61ba1fc930c594fd3f07a21f46f
zyq <zhouyq@egt365.com>
2018-06-05 18:01:28 +0800

    还原处理

--- src/main/java/com/hrtx/web/service/DictService.java
+++ src/main/java/com/hrtx/web/service/DictService.java
@@ -20,6 +20,7 @@ public class DictService {
 	private DictMapper dictMapper;
 
 	public Result pageDict(Dict dict) {
+		int a=1;
 		PageHelper.startPage(dict.getPageNum(),dict.getLimit());
 		Page<Object> ob=this.dictMapper.queryPageList(dict);
 		PageInfo<Object> pm = new PageInfo<Object>(ob);
