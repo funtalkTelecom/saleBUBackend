791bb36738f7125c945798bacab510b828c4004b
zyq <zhouyq@egt365.com>
2018-06-13 15:33:26 +0800

    test

--- src/test/java/zyq/ZyqTest.java
+++ src/test/java/zyq/ZyqTest.java
@@ -19,7 +19,7 @@ public class ZyqTest {
     @Test
     public void addUser()  {
         userService.paytest1();
-        int a = 2;
+        int a = 3;
         Assert.assertEquals("a","a");
     }
 }
