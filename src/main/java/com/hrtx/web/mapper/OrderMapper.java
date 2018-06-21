43c88ba18849a5a682da936fa9ba4f2202161068
htc <455096093@qq.com>
2018-06-21 15:21:02 +0800

    买家订单列表接口

--- src/main/java/com/hrtx/web/mapper/OrderMapper.java
+++ src/main/java/com/hrtx/web/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.hrtx.web.mapper;
 
 import com.github.abel533.mapper.Mapper;
+import com.github.pagehelper.Page;
 import com.hrtx.web.pojo.Order;
 import org.apache.ibatis.annotations.Param;
 
@@ -12,4 +13,6 @@ public interface OrderMapper extends Mapper<Order>,BaseMapper<Order>{
     void deleteByOrderid(@Param("orderid") Long orderId);
 
     Order findOrderInfo(@Param("orderid") Long id);
+
+    Page<Object> getOrderByConsumer(Order order);
 }
