d9384cab6280094a4465fdcf80d03377cfdbfa6c
zqf <zqf@egt365.com>
2018-06-23 15:58:18 +0800

    竟拍活动=》保证金支付中

--- src/main/java/com/hrtx/web/mapper/IccidMapper.java
+++ src/main/java/com/hrtx/web/mapper/IccidMapper.java
@@ -4,4 +4,5 @@ import com.github.abel533.mapper.Mapper;
 import com.hrtx.web.pojo.Iccid;
 
 public interface IccidMapper extends Mapper<Iccid>,BaseMapper<Iccid>{
+    void iccidEditStatus(Iccid iccid);
 }
