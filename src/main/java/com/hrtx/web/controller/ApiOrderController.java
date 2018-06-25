c36d61a17912f735daa155fa665e007d0d285119
zqf <zqf@egt365.com>
2018-06-25 16:17:38 +0800

    订单签收

--- src/main/java/com/hrtx/web/controller/ApiOrderController.java
+++ src/main/java/com/hrtx/web/controller/ApiOrderController.java
@@ -1,32 +1,16 @@
 package com.hrtx.web.controller;
 
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import com.hrtx.config.annotation.Powers;
-import com.hrtx.config.utils.RedisUtil;
 import com.hrtx.dto.Result;
-import com.hrtx.dto.StorageInterfaceRequest;
-import com.hrtx.global.*;
-import com.hrtx.web.dto.StorageInterfaceResponse;
-import com.hrtx.web.mapper.*;
-import com.hrtx.web.pojo.*;
-import com.hrtx.web.pojo.Number;
+import com.hrtx.global.PowerConsts;
+import com.hrtx.web.pojo.Order;
 import com.hrtx.web.service.ApiOrderService;
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.lang.System;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
 
 @RestController
 @RequestMapping("/api")
@@ -44,6 +28,14 @@ public class ApiOrderController extends BaseReturn{
     @ResponseBody
     public Result createOrder(HttpServletRequest request){
         return apiOrderService.createOrder(request, null);
+    }
+
+    @PostMapping("/orderSign")
+    @Powers(PowerConsts.NOPOWER)
+//    @Powers(PowerConsts.NOLOGINPOWER)
+    @ResponseBody
+    public Result signOrder(Order order,HttpServletRequest request){
+        return apiOrderService.signOrder(order,request);
     }
 
     @GetMapping("/order")
--- src/main/java/com/hrtx/web/service/ApiOrderService.java
+++ src/main/java/com/hrtx/web/service/ApiOrderService.java
@@ -3,7 +3,6 @@ package com.hrtx.web.service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.hrtx.config.annotation.Powers;
 import com.hrtx.config.utils.RedisUtil;
 import com.hrtx.dto.Result;
 import com.hrtx.global.*;
@@ -16,11 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.Number;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -51,6 +47,47 @@ public class ApiOrderService {
 	@Autowired
 	private RedisUtil redisUtil;
 
+
+	public Result signOrder(Order order,HttpServletRequest request)
+	{
+		Long orderId=0L;
+		orderId=order.getOrderId();
+		int status=0;
+		Long consumerId=0L;
+		if(orderId>0)
+		{
+			Order order2=orderMapper.selectByPrimaryKey(order.getOrderId());
+			if(order2!=null)
+			{
+				consumerId=order2.getConsumer();
+				if(!(this.apiSessionUtil.getConsumer().getId().toString().equals(consumerId.toString())))
+				{
+					return new Result(Result.ERROR, "该订单不属于当前用户");
+				}
+				if(!(order2.getStatus()==4))//4待签收(仓储物流已取件)；5完成
+				{
+					if(order2.getStatus()==5)
+					{
+						return new Result(Result.ERROR, "该订单处于完成状态，请选待签收状态的订单");
+					}else
+					{
+						return new Result(Result.ERROR, "该订单不是待签收状态的订单");
+					}
+				}
+			}else
+			{
+				return new Result(Result.ERROR, "该订单系统不存在");
+			}
+		}else
+		{
+			return new Result(Result.ERROR, "该订单系统不存在");
+		}
+		order.setStatus(5);
+	    order.setSignDate(new Date());//签收时间
+		order.setSignType(1);//签收方式1用户自动签收2系统
+		orderMapper.signByOrderid(order);
+		return new Result(Result.OK, "提交成功");
+	}
 
 	/**
 	 * 根据商品id创建订单
